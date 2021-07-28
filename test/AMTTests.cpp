23da243c34fd15994d2c5240a6e8fb168d9c5f4c
msterkowiec <83832773+msterkowiec@users.noreply.github.com>
2021-07-28 21:12:03 +0200

    First test for missing synchronization

--- test/AMTTests.cpp
+++ test/AMTTests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <atomic>
 #include "amt_vector.h"
 
 TEST(AMTTest, BasicTest) {
@@ -8,4 +9,32 @@ TEST(AMTTest, BasicTest) {
 	EXPECT_EQ(vec.size(), 0);
 	vec.emplace_back(10);
 	EXPECT_EQ(vec.size(), 1);
+}
+
+bool VectorUnsynchWriteTestFuncExcCaught[2] = {false, false};
+std::atomic<size_t> VectorUnsynchWriteTestThreadComplete = 0;
+
+void VectorUnsynchWriteTestFunc(size_t threadNo, amt::vector<int>& vec)
+{
+	try{
+		for (size_t i = 0 ; i < 32678 ; ++ i)
+			vec.push_back(i);
+	}
+	catch(amt::AMTCassertException& e)
+	{
+		VectorUnsynchWriteTestFuncExcCaught[threadNo] = true;
+	}
+	++ VectorUnsynchWriteTestThreadComplete;
+	return;
+}
+
+TEST(AMTTest, VectorUnsynchWriteTest) {
+	amt::SetThrowCustomAssertHandler<0>();
+	amt::vector<int> vec;
+	std::thread thread1(0, vec, &VectorUnsynchWriteTestFunc);
+	std::thread thread2(1, vec, &VectorUnsynchWriteTestFunc);
+	while (VectorUnsynchWriteTestThreadComplete != 2)
+		std::this_thread::yield();
+	EXPECT_EQ(VectorUnsynchWriteTestFuncExcCaught[0], true);
+	EXPECT_EQ(VectorUnsynchWriteTestFuncExcCaught[1], true);
 }
