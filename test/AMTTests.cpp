4466cb4906c53efd365371ad1bf0b669ff459c1e
msterkowiec <83832773+msterkowiec@users.noreply.github.com>
2021-07-29 09:24:14 +0200

    Fixed test VectorUnsynchWriteTest

--- test/AMTTests.cpp
+++ test/AMTTests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <thread>
+#include <mutex>
 #include <atomic>
 #include "amt_vector.h"
 
@@ -12,30 +13,37 @@ TEST(AMTTest, BasicTest) {
 	EXPECT_EQ(vec.size(), 1);
 }
 
-bool VectorUnsynchWriteTestFuncExcCaught[2] = {false, false};
+// ----------------------------------------------------------------------
+
+bool VectorUnsynchWriteTestFunc_AssertionFailed = false;
 std::atomic<size_t> VectorUnsynchWriteTest_ThreadsComplete;
+
+void VectorUnsynchWriteTest_CustomAssertHandler(bool a, const char* szFileName, long lLine, const char* szDesc)
+{
+	if (!a)
+		if(strstr(szDesc, "m_nPendingWriteRequests == 0") != nullptr) // make sure this the assertion we expect
+		{
+			VectorUnsynchWriteTestFunc_AssertionFailed = true;
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		}
+}
 
 void VectorUnsynchWriteTestFunc(size_t threadNo, amt::vector<int>& vec)
 {
-	try{
-		for (size_t i = 0 ; i < 32678 ; ++ i)
+	for (size_t i = 0; i < 32678 && !VectorUnsynchWriteTestFunc_AssertionFailed; ++i)
 			vec.push_back(i);
-	}
-	catch(amt::AMTCassertException& e)
-	{
-		VectorUnsynchWriteTestFuncExcCaught[threadNo] = true;
-	}
-	++ VectorUnsynchWriteTest_ThreadsComplete;
+	++VectorUnsynchWriteTest_ThreadsComplete;
 	return;
 }
 
+
 TEST(AMTTest, VectorUnsynchWriteTest) {
-	amt::SetThrowCustomAssertHandler<0>();
+	amt::SetCustomAssertHandler<0>(&VectorUnsynchWriteTest_CustomAssertHandler);
 	amt::vector<int> vec;
 	VectorUnsynchWriteTest_ThreadsComplete = 0;
 	std::thread thread1(&VectorUnsynchWriteTestFunc, 0, std::ref(vec));
 	std::thread thread2(&VectorUnsynchWriteTestFunc, 1, std::ref(vec));
 	while (VectorUnsynchWriteTest_ThreadsComplete != 2)
 		std::this_thread::yield();
-	EXPECT_EQ(VectorUnsynchWriteTestFuncExcCaught[0] || VectorUnsynchWriteTestFuncExcCaught[1], true);
+	 EXPECT_EQ(VectorUnsynchWriteTestFunc_AssertionFailed, true);
 }
