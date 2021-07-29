b41d2795b3dba86c0059e554e08680869a8305b4
msterkowiec <83832773+msterkowiec@users.noreply.github.com>
2021-07-29 14:57:46 +0200

    New test added

--- test/AMTTests.cpp
+++ test/AMTTests.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include <time.h> 
 #include "amt_vector.h"
+#include "amt_pod.h"
 
 TEST(AMTTest, BasicTest) {
 
@@ -13,6 +14,39 @@ TEST(AMTTest, BasicTest) {
 	EXPECT_EQ(vec.size(), 0);
 	vec.emplace_back(10);
 	EXPECT_EQ(vec.size(), 1);
+}
+
+// ----------------------------------------------------------------------
+// Test unsynchronized access to integer
+
+bool IntUnsynchWriteTest_AssertionFailed = false;
+
+void IntUnsynchWriteTest_CustomAssertHandler(bool a, const char* szFileName, long lLine, const char* szDesc)
+{
+	if (!a)
+		if(strstr(szDesc, "m_nPendingWriteRequests == 0") != nullptr) // make sure this the assertion we expect		
+			IntUnsynchWriteTest_AssertionFailed = true;
+}
+
+void IntUnsynchWriteTest_WriterThread(size_t threadNo, amt::int32_t& val)
+{
+	for (size_t i = 0; i < 32678 && !IntUnsynchWriteTest_AssertionFailed; ++i)
+		if (threadNo)
+			++ val;
+		else
+			-- val;
+	return;
+}
+
+TEST(AMTTest, IntUnsynchWriteTest) {
+	srand (time(NULL));
+	amt::SetCustomAssertHandler<0>(&IntUnsynchWriteTest_CustomAssertHandler);
+	amt::int32_t val;
+	std::thread thread1(&IntUnsynchWriteTest_WriterThread, 0, std::ref(val));
+	std::thread thread2(&IntUnsynchWriteTest_WriterThread, 1, std::ref(val));
+	thread1.join();
+	thread2.join();	
+	EXPECT_EQ(IntUnsynchWriteTest_AssertionFailed, true);
 }
 
 // ----------------------------------------------------------------------
