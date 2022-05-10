f2e0cd17f3ae8ef888900081e5853ab867200872
msterkowiec <83832773+msterkowiec@users.noreply.github.com>
2022-05-10 23:01:51 +0200

    Large set of fixes

--- src/amt_vector.h
+++ src/amt_vector.h
@@ -1,7 +1,7 @@
 //
 // Assertive MultiThreading Library
 //
-//  Copyright Marcin Sterkowiec, Piotr Tracz, 2021. Use, modification and
+//  Copyright Marcin Sterkowiec, Piotr Tracz, 2021-2022. Use, modification and
 //  distribution is subject to license (see accompanying file license.txt)
 //
 
@@ -17,7 +17,7 @@
 namespace amt
 {
 
-#if !defined(_DEBUG) && !defined(__AMT_RELEASE_WITH_ASSERTS__)
+#if !defined(__AMTL_ASSERTS_ARE_ON__)
 	template<typename T, class Alloc = std::allocator<T>, bool PREVENT_AMT_WRAP = false>
 	using vector = std::vector<T, Alloc>;
 #else
@@ -172,78 +172,146 @@ namespace amt
 		}
 		inline vector(const vector& o) : Base(o)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterReadingThread r(o);
-			Init();
+			Init();			
 			CRegisterWritingThread r2(*this);
+			#endif
 		}
 		inline vector(vector&& o) 
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(o);
 			Init();
 			CRegisterWritingThread r2(*this);
+			#endif
 			*((Base*)this) = std::move(*((Base*)&o));
+		}
+		template< class InputIt >
+		inline vector(InputIt begin, InputIt end)
+		{
+			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
+				Init();
+				CRegisterWritingThread r2(*this);
+				#endif
+			}
+			reserve(std::distance(begin, end));
+			for (auto it = begin; it != end; ++it)
+				push_back(*it);
+		}
+		inline vector(std::initializer_list<T> list)
+		{
+			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
+				Init();			
+				CRegisterWritingThread r(*this);			
+				#endif
+			}
+			reserve(list.size());
+			for (auto it = list.begin(); it != list.end(); ++it)
+				push_back(*it);
+		}
+		inline vector(size_t size)
+		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
+			Init();
+			CRegisterWritingThread r2(*this);
+			#endif
+			//((Base*)this)->resize(size, T(0));
+			ResizeWithInitHelper<ValueType, AllocatorType, std::is_scalar<T>::value>::resize(size, *((Base*)this)); // this trick is needed when wrapping up is on (__AMT_TRY_TO_AUTOMATICALLY_WRAP_UP_CONTAINERS_TYPES__)
+		}
+		inline vector(size_t size, const T& val)
+		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
+			Init();
+			CRegisterWritingThread r2(*this);
+			#endif
+			((Base*)this)->resize(size, val);
 		}
 		inline ~vector() __AMT_CAN_THROW__
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this);
 			AMT_CASSERT(m_nPendingReadRequests == 0);
 			AMT_CASSERT(m_nPendingWriteRequests == 1);
 			AMT_CASSERT(m_nPendingPartialReadRequests == 0);
 			AMT_CASSERT(m_nPendingPartialWriteRequests == 0);
+			#endif
 		}
 		__AMT_FORCEINLINE__ size_t size() const
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterReadingThread r(*this); // it is assumed to be full read in order to conflict with any time of write
+			#endif
 			return ((Base*)this)->size();
 		}
 		__AMT_FORCEINLINE__ size_t capacity() const
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterPartiallyReadingThread r(*this); 
+			#endif
 			return ((Base*)this)->capacity();
 		}
 		__AMT_FORCEINLINE__ void clear()
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this);
+			#endif
 			((Base*)this)->clear();
 		}
 		__AMT_FORCEINLINE__ bool empty() const
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterPartiallyReadingThread r(*this); 
+			#endif
 			return ((Base*)this)->empty();
 		}
 		__AMT_FORCEINLINE__ ValueType& operator[](size_t idx)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterPartiallyReadingThread r(*this);
+			#endif
 			AMT_CASSERT(idx < ((Base*)this)->size());
 			return ((Base*)this)->operator[](idx);
 		}
 		__AMT_FORCEINLINE__ const ValueType& operator[](size_t idx) const
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterPartiallyReadingThread r(*this);
+			#endif
 			AMT_CASSERT(idx < ((Base*)this)->size());
 			return ((Base*)this)->operator[](idx);
 		}
 		__AMT_FORCEINLINE__ ValueType& at(size_t idx)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterPartiallyReadingThread r(*this);
+			#endif
 			AMT_CASSERT(idx < ((Base*)this)->size());
 			return ((Base*)this)->at(idx);
 		}
 		__AMT_FORCEINLINE__ const ValueType& at(size_t idx) const
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterPartiallyReadingThread r(*this);
+			#endif
 			AMT_CASSERT(idx < ((Base*)this)->size());
 			return ((Base*)this)->at(idx);
 		}
 
 		inline ValueType* data() __AMT_NOEXCEPT__
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterPartiallyReadingThread r(*this);
+			#endif
 			return ((Base*)this)->data();
 		}
 		inline const ValueType* data() const __AMT_NOEXCEPT__
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterPartiallyReadingThread r(*this);
+			#endif
 			return ((Base*)this)->data();
 		}
 		
@@ -263,64 +331,110 @@ namespace amt
 		{
 			static void resize(size_t n, std::vector<U, AllocatorType>& vec)
 			{
-				vec.resize(n, 0);
+				vec.resize(n, U(0));
 			}
 		};
 
 	public:
+		__AMT_FORCEINLINE__ void reserve(size_t n)
+		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
+			CRegisterWritingThread r(*this);
+			#endif
+			((Base*)this)->reserve(n);
+		}
 		__AMT_FORCEINLINE__ void resize(size_t n)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this); // maybe to be verified later (PartiallyWriting under some circumstances?)
+			#endif
 			ResizeWithInitHelper<ValueType, AllocatorType, std::is_scalar<T>::value>::resize(n, *((Base*)this)); // this trick is needed when wrapping up is on (__AMT_TRY_TO_AUTOMATICALLY_WRAP_UP_CONTAINERS_TYPES__)
+			//((Base*)this)->resize(n, T(0));
 		}
 		__AMT_FORCEINLINE__ void resize(size_t n, const ValueType& val)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this); // maybe to be verified later (PartiallyWriting under some circumstances?)
+			#endif
 			((Base*)this)->resize(n, val);
 		}
 		__AMT_FORCEINLINE__ void shrink_to_fit()
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this);
+			#endif
 			((Base*)this)->shrink_to_fit();
 		}
 		__AMT_FORCEINLINE__ ValueType& front() const
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterPartiallyReadingThread r(*this);
+			#endif
 			return ((Base*)this)->front();
 		}
 		__AMT_FORCEINLINE__ ValueType& back() const
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterReadingThread r(*this); // partially?
+			#endif
 			return ((Base*)this)->back();
 		}
 		__AMT_FORCEINLINE__ void push_back(const T& t)
 		{
 			if (((Base*)this)->capacity() > ((Base*)this)->size())
 			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterPartiallyWritingThread r(*this);
+				#endif
 				((Base*)this)->push_back(t);
 			}
 			else
 			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterWritingThread r(*this);
+				#endif
 				((Base*)this)->push_back(t);
 			}
 		}
+		__AMT_FORCEINLINE__ void push_back(T&& t)
+		{
+			if (((Base*)this)->capacity() > ((Base*)this)->size())
+			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
+				CRegisterPartiallyWritingThread r(*this);
+				#endif
+				((Base*)this)->push_back(std::move(t));
+			}
+			else
+			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
+				CRegisterWritingThread r(*this);
+				#endif
+				((Base*)this)->push_back(std::move(t));
+			}
+		}
+
 		__AMT_FORCEINLINE__ void pop_back()
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this); // partially? however now, contrary to push_back without allocation, an element disappers, so cuncurrent read at the back() can fail
+			#endif
 			return ((Base*)this)->pop_back();
 		}
 		__AMT_FORCEINLINE__ void emplace_back(const T& t)
 		{
 			if (((Base*)this)->capacity() > ((Base*)this)->size())
 			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterPartiallyWritingThread r(*this);
+				#endif
 				return ((Base*)this)->push_back(t);
 			}
 			else
 			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterWritingThread r(*this);
+				#endif
 				return ((Base*)this)->push_back(t);
 			}
 		}
@@ -330,12 +444,16 @@ namespace amt
 		{
 			if (((Base*)this)->capacity() > ((Base*)this)->size())
 			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterPartiallyWritingThread r(*this);
+				#endif
 				return ((Base*)this)->emplace_back(args...);
 			}
 			else
 			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterWritingThread r(*this);
+				#endif
 				return ((Base*)this)->emplace_back(args...);
 			}
 		}
@@ -345,31 +463,41 @@ namespace amt
 		{
 			if (((Base*)this)->capacity() > ((Base*)this)->size())
 			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterPartiallyWritingThread r(*this);
+				#endif
 				 ((Base*)this)->emplace_back(args...);
 			}
 			else
 			{
+				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterWritingThread r(*this);
+				#endif
 				((Base*)this)->emplace_back(args...);
 			}
 		}
 		#endif
 		__AMT_FORCEINLINE__ void swap(vector& o)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this);
 			CRegisterWritingThread r2(o);
+			#endif
 			return ((Base*)this)->swap(*((Base*)&o));
 		}
 
 		__AMT_FORCEINLINE__ iterator erase(iterator it)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this);
+			#endif
 			return ((Base*)this)->erase(it);
 		}
 		__AMT_FORCEINLINE__ iterator erase(iterator itFirst, iterator itLast)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this);
+			#endif
 			return ((Base*)this)->erase(itFirst, itLast);
 		}
 
@@ -382,58 +510,74 @@ namespace amt
 		template <class InputIterator>
 		inline void assign(InputIterator first, InputIterator last)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this);
+			#endif
 			((Base*)this)->assign(first, last);
 		}
 		inline void assign(size_t n, const ValueType& val)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(*this);
+			#endif
 			((Base*)this)->assign(n, val);
 		}
 
 		__AMT_FORCEINLINE__ vector& operator = (const vector& o)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterReadingThread r(o);
 			CRegisterWritingThread r2(*this);
+			#endif
 			*((Base*)this) = *((Base*)&o);
 			return *this;
 		}
 		__AMT_FORCEINLINE__ vector& operator = (vector&& o)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterWritingThread r(o);
 			CRegisterWritingThread r2(*this);
+			#endif
 			*((Base*)this) = std::move(*((Base*)&o));
 			return *this;
 		}
 
 		__AMT_FORCEINLINE__ friend bool operator < (const vector& v1, const vector& v2)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterReadingThread r(v1);
 			CRegisterReadingThread r2(v2);
+			#endif
 			return *((Base*)&v1) < *((Base*)&v2);
 		}
 		__AMT_FORCEINLINE__ friend bool operator <= (const vector& v1, const vector&v2)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterReadingThread r(v1);
 			CRegisterReadingThread r2(v2);
+			#endif
 			return *((Base*)&v1) <= *((Base*)&v2);
 		}
 		__AMT_FORCEINLINE__ friend bool operator > (const vector& v1, const vector& v2)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterReadingThread r(v1);
 			CRegisterReadingThread r2(v2);
+			#endif
 			return *((Base*)&v1) > *((Base*)&v2);
 		}
 		__AMT_FORCEINLINE__ friend bool operator >= (const vector& v1, const vector& v2)
 		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
 			CRegisterReadingThread r(v1);
 			CRegisterReadingThread r2(v2);
+			#endif
 			return *((Base*)&v1) >= *((Base*)&v2);
 		}
 
 	};
 
-	// Partial specialization for vector of bool:
+	// TODO: Partial specialization for vector of bool:
 	template<class Alloc>
 	class vector<bool, Alloc> : public std::vector<bool, Alloc>
 	{
