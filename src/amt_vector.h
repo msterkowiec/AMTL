616f3e6739fa3bfbce9ae15e0891a6634f8e1c87
msterkowiec <83832773+msterkowiec@users.noreply.github.com>
2022-05-13 09:44:20 +0200

    Fix in emplace for missing std::forward

--- src/amt_vector.h
+++ src/amt_vector.h
@@ -447,14 +447,14 @@ namespace amt
 				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterPartiallyWritingThread r(*this);
 				#endif
-				return ((Base*)this)->emplace_back(args...);
+				return ((Base*)this)->emplace_back(std::forward<Args>(args)...);
 			}
 			else
 			{
 				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterWritingThread r(*this);
 				#endif
-				return ((Base*)this)->emplace_back(args...);
+				return ((Base*)this)->emplace_back(std::forward<Args>(args)...);
 			}
 		}
 		#else
@@ -466,17 +466,25 @@ namespace amt
 				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterPartiallyWritingThread r(*this);
 				#endif
-				 ((Base*)this)->emplace_back(args...);
+				((Base*)this)->emplace_back(std::forward<Args>(args)...);
 			}
 			else
 			{
 				#if __AMT_CHECK_MULTITHREADED_ISSUES__
 				CRegisterWritingThread r(*this);
 				#endif
-				((Base*)this)->emplace_back(args...);
+				((Base*)this)->emplace_back(std::forward<Args>(args)...);
 			}
 		}
 		#endif
+		template< class... Args >
+		__AMT_FORCEINLINE__ iterator emplace(const_iterator pos, Args&&... args)
+		{
+			#if __AMT_CHECK_MULTITHREADED_ISSUES__
+			CRegisterWritingThread r(*this);
+			#endif
+			return ((Base*)this)->emplace(pos, std::forward<Args>(args)...);
+		}
 		__AMT_FORCEINLINE__ void swap(vector& o)
 		{
 			#if __AMT_CHECK_MULTITHREADED_ISSUES__
