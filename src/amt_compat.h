8c21d42522c25261692631d26e7b9dfebd357d9f
msterkowiec <83832773+msterkowiec@users.noreply.github.com>
2022-05-15 14:51:34 +0200

    Temporary workaround for broken builds

--- src/amt_compat.h
+++ src/amt_compat.h
@@ -19,11 +19,11 @@
 #define __AMT_NOEXCEPT__ noexcept
 #define __AMT_NOEXCEPT_FALSE__ noexcept(false)
 #define __AMT_CONSTEXPR__ constexpr
-#if __cplusplus >= 201703L || (defined(_MSC_VER) && _MSVC_LANG >= 201703L)
+//#if __cplusplus >= 201703L || (defined(_MSC_VER) && _MSVC_LANG >= 201703L)
 #define __AMT_IF_CONSTEXPR__ constexpr
-#else
-#define __AMT_IF_CONSTEXPR__
-#endif
+//#else
+//#define __AMT_IF_CONSTEXPR__
+//#endif
 #endif
 
 #if __AMT_LET_DESTRUCTORS_THROW__
