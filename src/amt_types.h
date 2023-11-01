38f1e5323964b5e11cd0231cbc4beb015d9d333f
msterkowiec <83832773+msterkowiec@users.noreply.github.com>
2023-11-01 17:17:52 +0100

    Update amt_types.h

--- src/amt_types.h
+++ src/amt_types.h
@@ -70,7 +70,19 @@ namespace amt
 	{
 		typedef typename U::UnderlyingType type;
 	};
-	
+
+	template<typename T, typename = void>
+	struct is_iterator
+	{
+		static constexpr bool value = false;
+	};
+
+	template<typename T>
+	struct is_iterator<T, typename std::enable_if<!std::is_same<typename std::iterator_traits<T>::value_type, void>::value>::type>
+	{
+		static constexpr bool value = true;
+	};
+
 }
 
 #if __AMT_DEBUG__
